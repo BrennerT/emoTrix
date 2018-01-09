217fb48bb44006e86cda35e77050fe4d16393511
Torben Brenner <torben.brenner@gmail.com>
2018-01-09 10:57:06 +0100

    keyboard scanner page implemented

---
+++ ionic-app/src/pages/keyboard-scanner/keyboard-scanner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component ({
+    selector: 'keyboard-scanner',
+    templateUrl: 'keyboard-scanner.html'
+})
+export class KeyboardScannerPage {
+
+    constructor(public navCtrl: NavController){
+        
+    }
+
+}
