03be27415167bb125d0877a8a92d136a9fc63648
Torben Brenner <torben.brenner@gmail.com>
2018-02-19 11:39:33 +0100

    currently trying to emulate the app on android smartphone. Not working with this commit.

--- ionic-app/src/pages/keyboard-scanner/keyboard-scanner.ts
+++ ionic-app/src/pages/keyboard-scanner/keyboard-scanner.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Keyboard } from '@ionic-native/keyboard';
 
 @Component ({
     selector: 'keyboard-scanner',
-    templateUrl: 'keyboard-scanner.html'
+    templateUrl: 'keyboard-scanner.html',
+    providers: [Keyboard]
 })
 export class KeyboardScannerPage {
 
-    constructor(public navCtrl: NavController){
-        
+    constructor(public navCtrl: NavController, public keyboard: Keyboard){
+        this.keyboard.show()
     }
-
+    
 }
