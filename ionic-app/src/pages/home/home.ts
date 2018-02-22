ef067d10f1c875325590de2e4278ea459f0408ae
Torben Brenner <torben.brenner@gmail.com>
2018-02-22 14:23:05 +0100

    changed design of start page

--- ionic-app/src/pages/home/home.ts
+++ ionic-app/src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { KeyboardScannerPage } from './../keyboard-scanner/keyboard-scanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  tests: Array<{name: String, component: any}>;
+
   constructor(public navCtrl: NavController) {
-
+    this.tests = [
+      {name: "Keyboard Scanner", component: KeyboardScannerPage},
+      {name: "Test", component: HomePage}
+    ];
   }
-
+  
 }
