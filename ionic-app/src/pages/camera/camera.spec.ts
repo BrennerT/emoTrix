3f33839b67ba2486200fecef13c2be35af01a168
Torben Brenner <torben.brenner@googlemail.com>
2018-04-12 13:29:33 +0200

    added mini test

--- ionic-app/src/pages/camera/camera.spec.ts
+++ ionic-app/src/pages/camera/camera.spec.ts
@@ -1,24 +1,25 @@
 import { CameraPage } from './camera';
 import { Camera } from '@ionic-native/camera';
+
 describe("Camera Page", () => {
 
     describe("makeTest", () => {
 
         it("should take a picture", () => {
             const cameraMock = new Camera();
-            /*spyOn(cameraMock, "getPicture").and.callFake(() => {
-                return {
-                    then: (callback) => { return callback("picture")}  
-                };
-            });*/
-            spyOn(cameraMock, "getPicture");
+            spyOn(cameraMock, "getPicture").and.callThrough();
             let target = new CameraPage(cameraMock);
             target.makeTest();
             expect(cameraMock.getPicture).toHaveBeenCalled()
-            //expect(target.resultPic).toEqual("picture");
-
         })
 
+        it("should take the picture and set uri as latestPicture", () => {
+            const cameraMock = new Camera();
+            spyOn(cameraMock, "getPicture").and.returnValue="halloWelt.uri";
+            let target = new CameraPage(cameraMock);
+            target.makeTest();
+            expect(target.latestPicture).toEqual("halloWelt.uri");
+        })
     })
 
 })
