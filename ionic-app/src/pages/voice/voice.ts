87ff34b743dc7eedebd8181dae2febbe2da86f99
Torben Brenner <torben.brenner@googlemail.com>
2018-05-20 16:30:25 +0200

    added record button. does crash app after savign

--- ionic-app/src/pages/voice/voice.ts
+++ ionic-app/src/pages/voice/voice.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError } from '@ionic-native/media-capture';
 
 /**
  * Generated class for the VoicePage page.
@@ -15,11 +16,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VoicePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private mediaCapture: MediaCapture) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VoicePage');
+  }
+
+  onTestStart() {
+    this.mediaCapture.captureAudio()
+      .then(
+        (data: MediaFile[]) => {
+          console.log("Sucess");
+          console.log(data);
+        },
+        (error: CaptureError) => {
+          console.log("Error");
+          console.log(error);
+        }
+      )
   }
 
 }
